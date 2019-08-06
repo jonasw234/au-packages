ec8c8929f10a981380b2a5e6c2951d21c4547900
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2019-08-06 18:31:46 +0200

    Fix slow link search

--- ripgrep-all/update.ps1
+++ ripgrep-all/update.ps1
@@ -15,10 +15,10 @@ function global:au_GetLatest {
     $AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
     [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $url64   = $download_page.links | ? href -match '-x86_64-pc-windows-msvc.7z$' | % href | select -First 1
-    $version = (Split-Path ( Split-Path $url64 ) -Leaf).Substring(1)
+    $version = (Split-Path ( Split-Path $url64 ) -Leaf)
 
     @{
         URL64   = 'https://github.com' + $url64
