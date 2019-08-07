5dd29a8dc70e7ca1b709718b407d15d41cbae37d
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2019-08-07 21:33:26 +0200

    [AU watchexec:1.10.3]

--- watchexec/tools/chocolateyInstall.ps1
+++ watchexec/tools/chocolateyInstall.ps1
@@ -1,19 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName = $env:chocolateyPackageName
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://github.com/watchexec/watchexec/releases/download/1.10.3/watchexec-1.10.3-x86_64-pc-windows-gnu.zip'
-$checksum64  = 'F831908FBB0A2BE0A3A2DC10A5B2F26303FCCCB8'
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion"
 
 $packageArgs = @{
-    packageName    = $packageName
-    fileType       = 'ZIP'
-    url64bit       = $url64bit
-
-    checksum64     = $checksum64
-    checksumType64 = 'sha1'
-
-    unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+  packageName    = ''
+  url64Bit       = ''
+  checksum64     = ''
+  checksumType64 = 'sha256'
+  unzipLocation  = $toolsPath
 }
 
 Install-ChocolateyZipPackage @packageArgs
--- watchexec/update.ps1
+++ watchexec/update.ps1
@@ -5,8 +5,15 @@ $releases = 'https://github.com/watchexec/watchexec/releases'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*[$]url64\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
+            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
         }
     }
 }
@@ -17,10 +24,14 @@ function global:au_GetLatest {
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $url64   = $download_page.links | ? href -match '-x86_64-pc-windows-gnu.zip$' | % href | select -First 1
+    $url     = $download_page.links | ? href -match '-x86_64-pc-windows-gnu.zip$' | % href | select -First 1
     $version = (Split-Path ( Split-Path $url64 ) -Leaf)
 
-    return @{ URL64 = 'https://github.com' + $url64; Version = $version }
+    @{
+        Version      = $version
+        URL64        = $url
+        ReleaseNotes = ''
+    }
 }
 
 update
