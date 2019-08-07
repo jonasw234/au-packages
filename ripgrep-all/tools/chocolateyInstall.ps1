15b63ef50a41fc405819d6574599aec894b9cc63
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2019-08-07 22:28:58 +0200

    Use SHA256 instead

--- ripgrep-all/tools/chocolateyInstall.ps1
+++ ripgrep-all/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
     url64bit       = 'https://github.com/phiresky/ripgrep-all/releases/download/0.9.2/ripgrep_all-0.9.2-x86_64-pc-windows-msvc.7z'
     checksum64     = '9076633786EA4950F162F554E77E4D0FDFE90F2E95F08335F3ABB423A0CFC74E'
-    checksumType64 = 'sha1'
+    checksumType64 = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
