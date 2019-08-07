3f57cef0ad1d0869b3b22903848bc94f59df8190
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2019-08-07 20:53:33 +0200

    Removed variables in URLs
    This was a neat trick for manual packages, but search and replace needs
    the addresses hardcoded to replace them it seems.

--- gimagereader/tools/chocolateyInstall.ps1
+++ gimagereader/tools/chocolateyInstall.ps1
@@ -4,16 +4,16 @@ $version     = $env:chocolateyPackageVersion
 $packageName = $env:chocolateyPackageName
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url         = ("https://github.com/manisandro/gImageReader/releases/download/v{0}/gImageReader_{0}_qt5_i686.exe" -f $version)
-$url64       = ("https://github.com/manisandro/gImageReader/releases/download/v{0}/gImageReader_{0}_qt5_x86_64.exe" -f $version)
+$url         = ( -f $version)
+$url64       = ( -f $version)
 
 $packageArgs = @{
     fileType       = "exe"
     packageName    = $packageName
-    url            = $url
+    url            = "https://github.com/manisandro/gImageReader/releases/download/v3.3.1/gImageReader_3.3.1_qt5_i686.exe"
     checksum       = "2905d2b23c96073a9c762b828b4d3cac018a14e5"
     checksumType   = "sha1"
-    url64bit       = $url64
+    url64bit       = "https://github.com/manisandro/gImageReader/releases/download/v3.3.1/gImageReader_3.3.1_qt5_x86_64.exe"
     checksum64     = "6154e8332f2536419b52d2b74ff905789d4a7e9c"
     checksum64Type = "sha1"
     silentArgs     = "/S"
--- watchexec/tools/chocolateyInstall.ps1
+++ watchexec/tools/chocolateyInstall.ps1
@@ -4,12 +4,12 @@ $version     = $env:chocolateyPackageVersion
 $packageName = $env:chocolateyPackageName
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url64       = ("https://github.com/watchexec/watchexec/releases/download/{0}/watchexec-{0}-x86_64-pc-windows-gnu.zip" -f $version)
+$url64       = ( -f $version)
 
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64bit       = $url64
+    url64bit       = "https://github.com/watchexec/watchexec/releases/download/1.10.3/watchexec-1.10.3-x86_64-pc-windows-gnu.zip"
     checksum64     = "F831908FBB0A2BE0A3A2DC10A5B2F26303FCCCB8"
     checksumType64 = "sha1"
 }
