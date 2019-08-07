c7e20698779351b07e751807bcc123b33c7d1481
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2019-08-07 21:12:40 +0200

    Let's try this again

--- watchexec/tools/chocolateyInstall.ps1
+++ watchexec/tools/chocolateyInstall.ps1
@@ -1,15 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$version     = $env:chocolateyPackageVersion
 $packageName = $env:chocolateyPackageName
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64       = 'https://github.com/watchexec/watchexec/releases/download/1.10.3/watchexec-1.10.3-x86_64-pc-windows-gnu.zip'
+$checksum64  = 'F831908FBB0A2BE0A3A2DC10A5B2F26303FCCCB8'
 
 $packageArgs = @{
     packageName    = $packageName
-    unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64bit       = 'https://github.com/watchexec/watchexec/releases/download/1.10.3/watchexec-1.10.3-x86_64-pc-windows-gnu.zip'
-    checksum64     = 'F831908FBB0A2BE0A3A2DC10A5B2F26303FCCCB8'
+    fileType       = 'ZIP'
+    url64bit       = $url64
+
+    checksum64     = $checksum64
     checksumType64 = 'sha1'
+
+    unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
 }
 
 Install-ChocolateyZipPackage @packageArgs
