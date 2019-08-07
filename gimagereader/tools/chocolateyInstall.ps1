3b6a4abde82e646e46b102801429f9c72113e30e
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2019-08-07 22:34:52 +0200

    Use SHA256 instead

--- gimagereader/tools/chocolateyInstall.ps1
+++ gimagereader/tools/chocolateyInstall.ps1
@@ -8,11 +8,11 @@ $packageArgs = @{
     fileType       = 'exe'
     packageName    = $packageName
     url            = 'https://github.com/manisandro/gImageReader/releases/download/v3.3.1/gImageReader_3.3.1_qt5_i686.exe'
-    checksum       = '2905d2b23c96073a9c762b828b4d3cac018a14e5'
-    checksumType   = 'sha1'
+    checksum       = 'A363F282CF44E839C3163EFA00D363A7372A84432B396F380C2A50241F45DD44'
+    checksumType   = 'sha256'
     url64bit       = 'https://github.com/manisandro/gImageReader/releases/download/v3.3.1/gImageReader_3.3.1_qt5_x86_64.exe'
-    checksum64     = '6154e8332f2536419b52d2b74ff905789d4a7e9c'
-    checksum64Type = 'sha1'
+    checksum64     = '05F7A16A0CC4E914EF84BCF1246BFAB0636C15CC4A335220E7F8111D6A053F70'
+    checksum64Type = 'sha256'
     silentArgs     = '/S'
 }
 Install-ChocolateyPackage @packageArgs
