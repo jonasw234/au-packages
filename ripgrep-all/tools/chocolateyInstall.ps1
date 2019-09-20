f6f9e846d031f520f4a1b38f44bd0caa9d92b4de
Chocolatey <chocolatey@realdimensions.net>
2019-09-20 03:00:52 +0000

    AU: 1 updated - ripgrep-all [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/e8c123d134e3416a8820635fdcf2705dd0c38b0f

--- ripgrep-all/tools/chocolateyInstall.ps1
+++ ripgrep-all/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64bit       = 'https://github.com/phiresky/ripgrep-all/releases/download/0.9.2/ripgrep_all-0.9.2-x86_64-pc-windows-msvc.7z'
-    checksum64     = '9076633786EA4950F162F554E77E4D0FDFE90F2E95F08335F3ABB423A0CFC74E'
+    url64bit       = 'https://github.com/phiresky/ripgrep-all/releases/download/0.9.3/ripgrep_all-0.9.3-x86_64-pc-windows-msvc.7z'
+    checksum64     = 'ef26533e7a901755d50adc43c4cf34a267c5e008d1df2ccbae4d3cf8dadf7db3'
     checksumType64 = 'sha256'
 }
 
