3c629b7975ab0c206d4fe3ebe886a5569ffa2151
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2019-10-13 11:38:29 +0200

    [AU w10privacy]

--- w10privacy/tools/chocolateyInstall.ps1
+++ w10privacy/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion
 
 $packageArgs = @{
   packageName    = 'w10privacy'
-  url64Bit       = 'https://sf91b3285d9193eec.jimcontent.com/download/version/1567842684/module/12302828636/name/W10Privacy.zip'
-  checksum64     = '4D4FCBC0386BA52C63C530C1E1B80D13B78B3746C9D6B0DE0E850C8AB0969D51'
+  url64Bit       = 'https://sf91b3285d9193eec.jimcontent.com/download/version/1570770147/module/12302828636/name/W10Privacy.zip'
+  checksum64     = '2DB8973CB7AD5475D5AA80EEF9967A38E5873044C10C8195FC4D6C1E780F2E38'
   checksumType64 = 'sha256'
   unzipLocation  = $toolsPath
 }
