bd0d1e0f665565f39e3a2def005f1e7a77050636
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2020-02-23 23:04:12 +0100

    Add 0patch

---
+++ 0patch/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+$download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion"
+
+$packageArgs = @{
+  fileType       = 'msi'
+  packageName    = '0patch'
+  url            = 'https://dist.0patch.com/download/latestagent'
+  checksum       = ''
+  checksumType   = 'sha256'
+  silentArgs     = '/qn'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+Write-Warning "Don’t forget to put your login details into the 0patchConsole."
---
+++ 0patch/update.ps1
@@ -0,0 +1,24 @@
+import-module au
+. $PSScriptRoot\..\_scripts\all.ps1
+
+$releases = 'https://dist.0patch.com/download/latestagent'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $version = $download_page.Headers.'Content-Disposition'.Split('_')[1].Replace('.msi', '')
+
+    @{
+        Version = $version
+    }
+}
+
+update -ChecksumFor 32
