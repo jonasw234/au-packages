37023815cdedb628b2671d62027eebfc399456b6
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2020-03-22 22:34:23 +0100

    Create au package for Sleuthkit Autopsy

---
+++ autopsy/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:chocolateyPackageName
+
+$packageArgs = @{
+    packageName    = $packageName
+    fileType       = 'msi'
+    url            = 'https://github.com/sleuthkit/autopsy/releases/download/autopsy-4.14.0/autopsy-4.14.0-32bit.msi'
+    checksum       = '329c20a2487d81e059393ab524df564e70cd108d57c07e9b063f12d8d22767e4'
+    checksumType   = 'sha256'
+    url64bit       = 'https://github.com/sleuthkit/autopsy/releases/download/autopsy-4.14.0/autopsy-4.14.0-64bit.msi'
+    checksum64     = '47f8e1bc327dc1e858a6219bbe7377c5c0e64f8532706446ed882f336e5c3b4c'
+    checksumType64 = 'sha256'
+    silentArgs     = '/qn /norestart'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ autopsy/update.ps1
@@ -0,0 +1,33 @@
+import-module au
+
+$releases = 'https://github.com/sleuthkit/autopsy/releases/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url32   = $download_page.links | ? href -match '-32bit.msi$' | % href | select -First 1
+    $url64   = $download_page.links | ? href -match '-64bit.msi$' | % href | select -First 1
+    $version = (Split-Path ( Split-Path $url64 ) -Leaf).Substring(1)
+
+    @{
+        URL32   = 'https://github.com' + $url32
+        URL64   = 'https://github.com' + $url64
+        Version = $version
+    }
+}
+
+update
