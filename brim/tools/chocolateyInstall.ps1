2c9fd233a5a5de6e1aa28fef1e769a4e3a29d071
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2020-05-02 00:08:08 +0200

    Create package for Brim

---
+++ brim/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:chocolateyPackageName
+
+$packageArgs = @{
+  packageName  = $packageName
+  fileType     = 'exe'
+  url          = 'https://github.com/brimsec/brim/releases/download/v0.8.0/Brim-Setup.exe'
+  checksum     = '705FEE6E0F1302AC95B538E617BE0ED0E3054867660E69C855B6269122AAEB5E'
+  checksumType = 'sha256'
+  silentArgs   = '--silent'
+}
+
+Install-ChocolateyPackage @packageArgs
