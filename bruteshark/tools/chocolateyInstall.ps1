50aa500d81a1ceac6704e5aac0abc504e848dbe0
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2020-08-25 10:52:45 +0200

    Update BruteShark to 1.1.0, add new dependency

--- bruteshark/tools/chocolateyInstall.ps1
+++ bruteshark/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
 $packageName = $env:chocolateyPackageName
 
 $packageArgs = @{
   packageName    = 'bruteshark'
   fileType       = 'msi'
-  url64bit       = 'https://github.com/odedshimon/BruteShark/releases/download/v1.0.5/BruteSharkDesktopInstaller_x64.msi'
-  checksum64     = '8f8e5e66e3c08b5b42d11d65df2a2b48a1a73febfb60eee80e7b5bc7d61cbb63'
+  url64bit       = 'https://github.com/odedshimon/BruteShark/releases/download/v1.1.0/BruteSharkDesktopInstaller_x64.msi'
+  checksum64     = '5f9dc6b5a54da42313139fb0848ec0ee997c7f62039967ccfab03ec89251cbdf'
   checksumType64 = 'sha256'
   silentArgs     = '/qn /norestart'
 }
