9a8e5b0b83e5e9c7f9df72fa29ca39eaebb7aa5f
Chocolatey <chocolatey@realdimensions.net>
2020-08-27 03:00:47 +0000

    AU: 1 updated - espanso [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/f46cfe4f0a478ef6fad8f878ca222ae9f5ab8a7b

--- espanso/tools/chocolateyInstall.ps1
+++ espanso/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://github.com/federico-terzi/espanso/releases/download/v0.7.0/espanso-win-installer.exe'
-  checksum       = 'bc747c6ea170ada5c89558f5d31c04ed55e0d98fc3f2faa575b722a90411d89b'
+  url            = 'https://github.com/federico-terzi/espanso/releases/download/v0.7.1/espanso-win-installer.exe'
+  checksum       = '90f469cdde1c88da8644feeddcdf528bc5fcb315d9c20cc5ea086a62e87a8cf5'
   checksumType   = 'sha256'
   silentArgs     = '/verysilent /norestart /nocloseapplications'
 }
