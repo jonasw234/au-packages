0dfa8255afb7bccc7ccf644d1453ea476c2ecd46
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2020-11-16 12:37:28 +0100

    Uninstall BruteShark before updating

--- bruteshark/tools/chocolateyInstall.ps1
+++ bruteshark/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageName = $env:chocolateyPackageName
 
 $packageArgs = @{
-  packageName    = 'bruteshark'
+  packageName    = $packageName
   fileType       = 'msi'
   url64bit       = 'https://github.com/odedshimon/BruteShark/releases/download/v1.1.1/BruteSharkDesktopInstaller_x64.msi'
   checksum64     = '83203976bcede808b57eaae4c7148b2ed170f6c6d021e2a93fcc492e26c10fa2'
@@ -11,4 +11,9 @@ $packageArgs = @{
   silentArgs     = '/qn /norestart'
 }
 
+$uninstall_key = Get-UninstallRegistryKey -SoftwareName 'BruteSharkDesktop'
+if ($uninstall_key) {
+    # BruteShark needs to be uninstalled before updating
+    Uninstall-ChocolateyPackage $packageName
+}
 Install-ChocolateyPackage @packageArgs
