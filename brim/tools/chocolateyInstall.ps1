d73f89fa1ff948b6dbbf8f8e776b3358f75bba70
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-01-03 23:00:37 +0100

    Add missing checksum

--- brim/tools/chocolateyInstall.ps1
+++ brim/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
   url          = 'https://github.com/brimsec/brim/releases/download/v0.21.1/Brim-Setup.exe'
-  checksum     = ''
+  checksum     = '6a52e2f274ffa35349ad366421e657cf300672c50802daa5e48ec8bf99f6071b'
   checksumType = 'sha256'
   silentArgs   = '--silent'
 }
