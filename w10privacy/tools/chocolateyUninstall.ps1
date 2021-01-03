036aa22b84dbc997c13b6eef554f4c70c93ed2d4
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-01-03 22:56:57 +0100

    Run uninstaller on uninstall

--- w10privacy/tools/chocolateyUninstall.ps1
+++ w10privacy/tools/chocolateyUninstall.ps1
@@ -1,2 +1,2 @@
-Write-Host Uninstall-ChocolateyPackage -PackageName 'w10privacy' -FileType 'exe' -SilentArgs '/S' -File "$env:ProgramFiles(x86)\W10Privacy\Uninstall.exe"
+Uninstall-ChocolateyPackage -PackageName 'w10privacy' -FileType 'exe' -SilentArgs '/S' -File "$env:ProgramFiles(x86)\W10Privacy\Uninstall.exe"
 Uninstall-ChocolateyZipPackage -PackageName 'w10privacy' -ZipFileName 'W10Privacy.zip'
