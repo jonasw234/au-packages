6d1acb3d0a80076a6c1f6874f1de86e55411091d
Chocolatey <chocolatey@realdimensions.net>
2021-01-09 03:00:49 +0000

    AU: 1 updated - bruteshark [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7

--- bruteshark/tools/chocolateyInstall.ps1
+++ bruteshark/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'msi'
-  url64bit       = 'https://github.com/odedshimon/BruteShark/releases/download/v1.1.3/BruteSharkDesktopInstaller_x64.msi'
-  checksum64     = '89da430e30fef9a4ac020e193eb37ab21fb55b4ca4f2301b2845eb2cbc649862'
+  url64bit       = 'https://github.com/odedshimon/BruteShark/releases/download/v1.1.4/BruteSharkDesktopInstaller_x64.msi'
+  checksum64     = '1751c12fca8c3615b7f27fe8464c453c2af71ede9103c363f98d59a5cba07bd5'
   checksumType64 = 'sha256'
   silentArgs     = '/qn /norestart'
 }
