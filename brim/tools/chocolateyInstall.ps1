96ca4a6501da6fee629db29e85cc05607dffdf24
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-01-18 20:28:44 +0100

    Add checksum to Brim

--- brim/tools/chocolateyInstall.ps1
+++ brim/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
   url          = 'https://github.com/brimsec/brim/releases/download/v0.22.0/Brim-Setup.exe'
-  checksum     = ''
+  checksum     = '7e1405121612b524798e77a930cc5edfe174c8713e791ef219cb09f87203cd5e'
   checksumType = 'sha256'
   silentArgs   = '--silent'
 }
