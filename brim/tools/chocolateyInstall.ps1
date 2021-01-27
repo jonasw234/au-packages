cf796e80b13e5997bf6ba8b4411efa4ae71208bd
Chocolatey <chocolatey@realdimensions.net>
2021-01-27 03:01:01 +0000

    AU: 1 updated - brim [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7

--- brim/tools/chocolateyInstall.ps1
+++ brim/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageName = $env:chocolateyPackageName
 
 $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
-  url          = 'https://github.com/brimsec/brim/releases/download/v0.22.0/Brim-Setup.exe'
-  checksum     = '7e1405121612b524798e77a930cc5edfe174c8713e791ef219cb09f87203cd5e'
+  url          = 'https://github.com/brimsec/brim/releases/download/v0.23.0/Brim-Setup.exe'
+  checksum     = ''
   checksumType = 'sha256'
   silentArgs   = '--silent'
 }
