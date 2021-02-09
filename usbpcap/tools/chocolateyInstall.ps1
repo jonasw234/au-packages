20cd6748198ea0791175e7739e53fb5e3d27aa36
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-02-09 20:39:13 +0100

    Add USBPcap

---
+++ usbpcap/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:chocolateyPackageName
+
+$packageArgs = @{
+  packageName  = $packageName
+  fileType     = 'exe'
+  url          = 'https://github.com/desowin/usbpcap/releases/download/1.5.4.0/USBPcapSetup-1.5.4.0.exe'
+  checksum     = '87a7edf9bbbcf07b5f4373d9a192a6770d2ff3add7aa1e276e82e38582ccb622'
+  checksumType = 'sha256'
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
