cbf758262abe882b5dedf9563702ecdd88d76ba8
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-02-14 01:45:00 +0100

    Update package and fix typo in nuspec name

--- amongus-extraroles/tools/chocolateyInstall.ps1
+++ amongus-extraroles/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageName = $env:chocolateyPackageName
 $gameDirectory = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 945360\').InstallLocation
@@ -7,8 +7,8 @@ $modDirectory = ($gameDirectory + " - Extra Roles Mod")
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'zip'
-  url           = 'https://github.com/NotHunter101/ExtraRolesAmongUs/releases/download/v1.2.6/Role.Mod.v.1.2.6.zip'
-  checksum      = '30c1a22e26747a75f86985de655ac84a09bf51d8c26460652bcf3a9cb7c00d4f'
+  url           = 'https://github.com/NotHunter101/ExtraRolesAmongUs/releases/download/v1.3.0/Role.Mod.v.1.3.0.zip'
+  checksum      = ''
   checksumType  = 'sha256'
   unzipLocation = $modDirectory
 }
