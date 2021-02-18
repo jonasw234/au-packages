dddb53bfcea924382a29074b0a146247974b284d
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-02-18 10:51:47 +0100

    Fix checksum (hopefully for good this time!)

--- brim/tools/chocolateyInstall.ps1
+++ brim/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
   url          = 'https://github.com/brimsec/brim/releases/download/v0.24.0/Brim-Setup.exe'
-  checksum     = ''
+  checksum     = '5d2a4c947659c300f047e3b3870a9bc19b81d5ce7cd47a68d74923d2ee46dbee'
   checksumType = 'sha256'
   silentArgs   = '--silent'
 }
