22092d601e2e10a7015af26ae6f54ff25424b6e1
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-05-29 14:29:09 +0200

    Fix updater

--- amongus-extraroles/tools/chocolateyInstall.ps1
+++ amongus-extraroles/tools/chocolateyInstall.ps1
@@ -1,24 +1,32 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = $env:chocolateyPackageName
-$gameDirectory = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 945360\').InstallLocation
-$modDirectory = ($gameDirectory + " - Extra Roles Mod")
+$gameDirectory = ''
+try {
+    $gameDirectory = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 945360\').InstallLocation
+    $modDirectory = ($gameDirectory + " - Extra Roles Mod")
+} catch {
+    Write-Error "Cannot locate game directory.  This package works only with the Steam version of Among Us!"
+}
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'zip'
-  url           = 'https://github.com/NotHunter101/ExtraRolesAmongUs/releases/download/v1.3.0/Role.Mod.v.1.3.0.zip'
-  checksum      = '9ef048f0d664e499ec06fe29391819359c2689d87eb898a173fbc0f37e674a4b'
+  url           = 'https://github.com/NotHunter101/ExtraRolesAmongUs/releases/download/v1.3.1/Role.Mod.v.1.3.1.zip'
+  checksum      = '57749791023660c3149abfacb01b68675cb71562a5db18a306e4947fc322c59b'
   checksumType  = 'sha256'
   unzipLocation = $modDirectory
 }
-If (!(Test-Path $modDirectory))
+If ($gameDirectory)
 {
-    Copy-Item -Recurse $gameDirectory $modDirectory
-}
-Else
-{
-    Write-Warning "$modDirectory already exists.  Unpacking Extra Roles mod there."
-}
+    If (!(Test-Path $modDirectory))
+    {
+        Copy-Item -Recurse $gameDirectory $modDirectory
+    }
+    Else
+    {
+        Write-Warning "$modDirectory already exists.  Unpacking Extra Roles mod there."
+    }
 
-Install-ChocolateyZipPackage @packageArgs
+    Install-ChocolateyZipPackage @packageArgs
+}
