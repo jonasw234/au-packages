b9bf1073c6433218a9b71e3b7c7890358e07ddd0
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-11-07 12:27:24 +0100

    Brim installer changed their file name scheme
    This new regex should be more robust to changes like these.
    Previous: Brim-Setup.exe
    Now: Brim-Setup-VERSION.exe

--- brim/update.ps1
+++ brim/update.ps1
@@ -18,7 +18,7 @@ function global:au_GetLatest {
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re      = 'Brim-Setup.exe'
+    $re      = 'Brim-Setup.*\.exe'
     $domain  = $releases -split '(?<=//.+)/' | Select-Object -First 1
     $url     = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
     $url     = $url | ForEach-Object {$domain + $_ }
