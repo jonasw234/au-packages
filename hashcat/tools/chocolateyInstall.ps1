735c0dbedca7022d53e9201477b57b371c4d540b
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-11-16 18:05:24 +0100

    Added rest of files for hashcat package

---
+++ hashcat/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:chocolateyPackageName
+
+$packageArgs = @{
+    packageName    = $packageName
+    unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+    url64Bit       = 'https://github.com/hashcat/hashcat/releases/download/v6.2.4/hashcat-6.2.4.7z'
+    checksum64     = 'be6abeca5ab5b06a17850c69e954e7124d91ecdb5844b75e4df548158cbc9514'
+    checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ hashcat/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+$releases = 'https://github.com/hashcat/hashcat/releases'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64Bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url     = $download_page.links | Where-Object href -match 'hashcat-(\d\.)+\d\.7z$' | ForEach-Object href | Select-Object -First 1
+    $version = $url -split '-v|/hashcat-(\d\.)+\d\.7z'
+    $version = $version[3]
+
+    return @{
+        URL     = 'https://github.com' + $url
+        Version = $version
+    }
+}
+
+update -ChecksumFor All
