66018c1358bc48cd467358f05ac5aa8b1a8b8a9f
Chocolatey <chocolatey@realdimensions.net>
2021-11-23 09:25:08 +0000

    AU: 1 updated - czkawka [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/5d8124da0f2f7603621f6c1f3cfad913cd503c66

--- czkawka/tools/chocolateyInstall.ps1
+++ czkawka/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64Bit       = 'https://github.com/qarmin/czkawka/releases/download/3.2.0/windows_czkawka_gui.zip'
-    checksum64     = '260f80c56807680859f75edd32e8e34d95cb7dd0b5254052228efe057abf00af'
+    url64Bit       = 'https://github.com/qarmin/czkawka/releases/download/3.3.1/windows_czkawka_gui.zip'
+    checksum64     = ''
     checksumType64 = 'sha256'
 }
 
