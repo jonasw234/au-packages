02c2b85d538859f107703d5c005a4cc18f24131b
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-11-23 12:19:30 +0100

    Fixed silent installer arguments

--- brim/tools/chocolateyInstall.ps1
+++ brim/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   url          = 'https://github.com/brimdata/brim/releases/download/v0.28.0/Brim-Setup-0.28.0.exe'
   checksum     = '363fe8954edb1e826d2932d779973293479274a813fd7b5c0dfb67f8732ca9fd'
   checksumType = 'sha256'
-  silentArgs   = '--silent'
+  silentArgs   = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs
