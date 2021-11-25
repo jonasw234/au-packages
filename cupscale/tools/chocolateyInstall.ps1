6b6f67ebb049dfddc16bf17ed79709fa301c610c
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-11-25 11:52:29 +0100

    Prepare Cupscale

---
+++ cupscale/tools/chocolateyInstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:chocolateyPackageName
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$pp = Get-PackageParameters
+$installDir = $toolsPath
+if ($pp.InstallDir) {
+    $InstallDir = $pp.InstallDir
+}
+Write-Host "Cupscale is going to be installed in '$installDir'"
+
+$packageArgs = @{
+    packageName    = $packageName
+    unzipLocation  = $installDir
+    url64Bit       = 'https://github.com/n00mkrad/cupscale/releases/download/1.39/Cupscale.1.39.0f1.zip'
+    checksum64     = 'fde68914ea4a103f4fe6cdc95a264b1b3af734bc768762ff6d7cfe22feb95e18'
+    checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+if ($installDir -ne $toolsPath) {
+    Install-ChocolateyPath $installDir
+}
---
+++ cupscale/update.ps1
@@ -0,0 +1,31 @@
+import-module au
+
+$releases = 'https://github.com/n00mkrad/cupscale/releases'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64Bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $file    = 'cupscale[^/]+\.zip$'
+    $url     = $download_page.links | Where-Object href -match $file | ForEach-Object href | Select-Object -First 1
+    $version = $url -split '\/download\/|\/' + $file
+    $version = $version[1]
+
+    return @{
+        URL     = 'https://github.com' + $url
+        Version = $version
+    }
+}
+
+update -ChecksumFor 64
