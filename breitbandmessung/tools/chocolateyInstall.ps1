6c6addb905ac69e1368eab2ab4fb86168384d820
Chocolatey <chocolatey@realdimensions.net>
2021-12-14 03:01:01 +0000

    AU: 1 updated - breitbandmessung [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/5e6c23ed547c9ff8041711853f1f8a36d7a726ec

--- breitbandmessung/tools/chocolateyInstall.ps1
+++ breitbandmessung/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
   url          = 'https://download.breitbandmessung.de/bbm/Breitbandmessung-win.exe'
-  checksum     = 'adc86f0359dd97c7eb751584c82b5486c2215ebf6e15ca02a07a9e4b6c5d44b5'
+  checksum     = '21efe82c36bc04ba0a95a6a2f4616c251268b1cb9533ecc32669124f37377590'
   checksumType = 'sha256'
   silentArgs   = '/S'
 }
