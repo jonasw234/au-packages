b91ad2c3aca488533fc2bc9ec2f9991dfc09e6c0
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-12-19 12:30:06 +0100

    Add checksum

--- watchexec/tools/chocolateyInstall.ps1
+++ watchexec/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
     url64Bit       = 'https://github.com/watchexec/watchexec/releases/download/cli-v1.17.1/watchexec-1.17.1-x86_64-pc-windows-msvc.zip'
-    checksum64     = ''
+    checksum64     = 'a39e5d711c93b3fd2d08b9196573f727cfdeec70a95f5b0ae4d0a9201a6ab14c'
     checksumType64 = 'sha256'
 }
 
