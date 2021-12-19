2e15e6d468a8a6ae3ca2ad0ed1855864d8313473
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-12-19 12:27:42 +0100

    Add checksum

--- czkawka/tools/chocolateyInstall.ps1
+++ czkawka/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
     url64Bit       = 'https://github.com/qarmin/czkawka/releases/download/3.3.1/windows_czkawka_gui.zip'
-    checksum64     = ''
+    checksum64     = '2697e71a8aa0242c35293460fe0f625c113aa9c518a8010313d94ebb1f7a64cd'
     checksumType64 = 'sha256'
 }
 
