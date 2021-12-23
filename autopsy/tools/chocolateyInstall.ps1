d2a2ce2bb0898d09ace96c326f470d0b7b51185e
Chocolatey <chocolatey@realdimensions.net>
2021-12-23 03:02:34 +0000

    AU: 1 updated - autopsy [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/226ff09e57ebbad243388c538bceda88f6375636

--- autopsy/tools/chocolateyInstall.ps1
+++ autopsy/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
     url            = 'https://github.com/sleuthkit/autopsy/releases/download/autopsy-4.19.2/autopsy-4.19.2-32bit.msi'
     checksum       = 'f479ef83259333ee20774b9d98803a4f66da2d7d6ccb3b93c7af3fe695c48c2e'
     checksumType   = 'sha256'
-    url64bit       = 'https://github.com/sleuthkit/autopsy/releases/download/autopsy-4.19.2/autopsy-4.19.2-64bit.msi'
-    checksum64     = '854bb89cfc12e3cfe844a8bfb40e6a876fbf55732088219bace4578d7fd0c0fe'
+    url64bit       = 'https://github.com/sleuthkit/autopsy/releases/download/autopsy-4.19.3/autopsy-4.19.3-64bit.msi'
+    checksum64     = 'c61beb6ad0ccb6182c9af376fd74b12387f6bfac515bada249f8c9ca96f9cb72'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
 }
