a9e15a278b6fdc1f44a4cc0de6e198c2c3a0567a
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2021-12-26 22:34:47 +0100

    According to their documentation, APPLICATIONFOLDER is necessary

--- freeoffice/tools/chocolateyInstall.ps1
+++ freeoffice/tools/chocolateyInstall.ps1
@@ -1,6 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = $env:chocolateyPackageName
+$pp = Get-PackageParameters
+$installDir = 'C:\Program Files\SoftMaker FreeOffice 2021'
+if ($pp.InstallDir) {
+    $installDir = $pp.InstallDir
+}
 
 $packageArgs = @{
     packageName    = $packageName
@@ -8,7 +13,7 @@ $packageArgs = @{
     url            = 'https://www.freeoffice.com/download.php?filename=https://www.softmaker.net/down/freeoffice2021.msi'
     checksum       = 'ab128a576a4fd5ae51484178201b634ab306d2c17d341c30e59f8aec59b69f76'
     checksumType   = 'sha256'
-    silentArgs     = '/qn'
+    silentArgs     = '/qn APPLICATIONFOLDER="' + $installDir + '"'
 }
 
 Install-ChocolateyPackage @packageArgs
