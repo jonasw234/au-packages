91991b7530ce65e45105170dfe66ab3a957326b5
Chocolatey <chocolatey@realdimensions.net>
2022-01-06 03:01:19 +0000

    AU: 1 updated - obsidian [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/790e0b3a43a9344a9eaa25eb594daa39fbdfc8b5

--- obsidian/tools/chocolateyInstall.ps1
+++ obsidian/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
-  url          = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v0.13.14/Obsidian.0.13.14.exe'
-  checksum     = 'df82ed43c3f9aed3c1fe2d39516005403aca91304bac2ecd32425a9315ab5836'
+  url          = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v0.13.19/Obsidian.0.13.19.exe'
+  checksum     = '3e6d589a66d9c977c69cca28467ffa534dd5bc069851a82230af17a96ce44543'
   checksumType = 'sha256'
   silentArgs   = '/S'
 }
