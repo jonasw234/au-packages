aeec5980ff2faebba9bbf7ec8aa584e6190748a9
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2022-03-09 17:49:36 +0100

    New 64 bit alpha version

--- espanso/tools/chocolateyInstall.ps1
+++ espanso/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://github.com/federico-terzi/espanso/releases/download/v0.7.3/espanso-win-installer.exe'
-  checksum       = '5b19d107601f736a072610f926044f5bae537590f47d67bfd358b8652d261c98'
-  checksumType   = 'sha256'
+  url64          = 'https://github.com/federico-terzi/espanso/releases/download/v2.1.3-alpha/Espanso-Win-Installer-x86_64.exe'
+  checksum64     = '5c3b5b4c14671d0fb56219499d12f10241680d3e996f92f84576274655b381f0'
+  checksumType64 = 'sha256'
   silentArgs     = '/verysilent /norestart /nocloseapplications'
 }
 
--- espanso/update.ps1
+++ espanso/update.ps1
@@ -5,8 +5,8 @@ $releases = 'https://github.com/federico-terzi/espanso/releases'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -17,13 +17,13 @@ function global:au_GetLatest {
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $url32   = $download_page.links | Where-Object href -match '-win-installer.exe$' | Select-Object -First 1 -expand href
-    $version = $url32.split('/v')[6]
+    $url64   = $download_page.links | Where-Object href -match '-win-installer' | Select-Object -First 1 -expand href
+    $version = $url64.split('/v')[6]
 
     @{
-        URL32   = 'https://github.com' + $url32
+        URL64   = 'https://github.com' + $url64
         Version = $version
     }
 }
 
-Update-Package -ChecksumFor all
+Update-Package -ChecksumFor 64
