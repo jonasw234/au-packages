28b5a394c0aa72bf5beb1bf430db7aa565bb1b43
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2022-03-26 12:31:14 +0100

    Don’t restart automatically

--- freeoffice/tools/chocolateyInstall.ps1
+++ freeoffice/tools/chocolateyInstall.ps1
@@ -13,7 +13,7 @@ $packageArgs = @{
     url            = 'https://www.freeoffice.com/download.php?filename=https://www.softmaker.net/down/freeoffice2021.msi'
     checksum       = 'a16c3fb1dc8dc180c0378d999d7e35b0a3ffb6f72334dd771cc42fa64fc77223'
     checksumType   = 'sha256'
-    silentArgs     = '/qn APPLICATIONFOLDER="' + $installDir + '"'
+    silentArgs     = '/norestart /qn APPLICATIONFOLDER="' + $installDir + '"'
 }
 
 Install-ChocolateyPackage @packageArgs
