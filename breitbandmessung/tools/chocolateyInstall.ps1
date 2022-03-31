5a3e39618e1e65d42bcf7c9523b1a3b823d6f6db
Chocolatey <chocolatey@realdimensions.net>
2022-03-31 03:01:16 +0000

    AU: 2 updated - breitbandmessung espanso [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/5ee1bc670be30f6516e01098eb757fd15b19f268

--- breitbandmessung/tools/chocolateyInstall.ps1
+++ breitbandmessung/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
   url          = 'https://download.breitbandmessung.de/bbm/Breitbandmessung-win.exe'
-  checksum     = '3642d47db20b12eac796288b51a69e3176d214e5f6a710288533f83622b9777c'
+  checksum     = '1a6c6dc6d5a940b7b5b966ec7302ef1c6cb13a44f747b1126cf6deed42582cdb'
   checksumType = 'sha256'
   silentArgs   = '/S'
 }
--- espanso/tools/chocolateyInstall.ps1
+++ espanso/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url64          = 'https://github.com/federico-terzi/espanso/releases/download/v2.1.4-beta/Espanso-Win-Installer-x86_64.exe'
-  checksum64     = 'ffc485eb8e60607192e29a38659a4b930dfd69d42bbe38657525eb505bdf65cc'
+  url64          = 'https://github.com/federico-terzi/espanso/releases/download/v2.1.5-beta/Espanso-Win-Installer-x86_64.exe'
+  checksum64     = 'a3da8f3b721405a34c095d6f8e553518e140311e02c512453898e55dbd4f3e70'
   checksumType64 = 'sha256'
   silentArgs     = '/verysilent /norestart /nocloseapplications'
 }
