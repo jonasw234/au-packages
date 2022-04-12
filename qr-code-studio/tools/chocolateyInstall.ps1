f2eb36eb75e97fe23db0e2f3f396eb4d2b2f867f
Chocolatey <chocolatey@realdimensions.net>
2022-04-12 03:01:16 +0000

    AU: 2 updated - obsidian qr-code-studio [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/a0ed2e2ad2ef93eb87541244542024c78590e22d

--- qr-code-studio/tools/chocolateyInstall.ps1
+++ qr-code-studio/tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
   url          = 'https://www.tec-it.com/download/QRCode_Studio.exe'
-  checksum     = '204ce8de0961d1fe10087065c8c7ec56e870b1431f372b68007cdbdb595c4407'
+  checksum     = '9d7e66c2d308ce8c7ae6c4600b86c7b5bec0441398cfee4b12f51a1e222dfe71'
   checksumType = 'sha256'
   silentArgs   = '/quiet'
 }
