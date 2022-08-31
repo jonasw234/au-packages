29d187cd0c978199ba94c94869377c939ccdbc57
Chocolatey <chocolatey@realdimensions.net>
2022-08-31 03:00:55 +0000

    AU: 1 updated - czkawka [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/ad20a70ad931faf3348c6c21bdbd4da90f292a08

--- czkawka/tools/chocolateyInstall.ps1
+++ czkawka/tools/chocolateyInstall.ps1
@@ -10,8 +10,8 @@ $iconPath               = Join-Path $packageToolsDir 'icon_about.ico'
 $desktopShortcutPath    = "$($ENV:PUBLIC)\Desktop\Czkawka GUI.lnk"
 $startmenuShortcutPath  = "$($ENV:ProgramData)\Microsoft\Windows\Start Menu\Programs\Czkawka GUI.lnk"
 
-$url64                  = 'https://github.com/qarmin/czkawka/releases/download/5.0.1/windows_czkawka_gui.zip'
-$checksum64             = 'b97267b5faab76265a39d68c62e4a367677d488235308c2f95ea975e64109fba'
+$url64                  = 'https://github.com/qarmin/czkawka/releases/download/5.0.2/windows_czkawka_gui.zip'
+$checksum64             = '52bc7ecd3b541ffd5918fd265724396909420fa73bd678579aef164a550c63b5'
 $checksumType64         = 'sha256'
 
 $packageName = $env:chocolateyPackageName
