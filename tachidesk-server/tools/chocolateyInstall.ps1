fa7a53f01e54cbb54fa07f8bceddb0cb38ce3bb2
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2022-11-09 20:07:29 +0100

    Add Tachidesk-Server

---
+++ tachidesk-server/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:chocolateyPackageName
+
+$packageArgs = @{
+    packageName    = $packageName
+    fileType       = 'msi'
+    url            = 'https://github.com/Suwayomi/Tachidesk-Server/releases/download/v0.6.5/Tachidesk-Server-v0.6.5-r1122-windows-x86.msi'
+    checksum       = 'be05c88df93682d427bda713c47f9601cf9126fa61c0702bc22445ff41048df2'
+    checksumType   = 'sha256'
+    url64bit       = 'https://github.com/Suwayomi/Tachidesk-Server/releases/download/v0.6.5/Tachidesk-Server-v0.6.5-r1122-windows-x64.msi'
+    checksum64     = '0a5bfbc4ef1a22fd903adb3de4f81f893eeddd170b844b7f7d5d83af6c9462e1'
+    checksumType64 = 'sha256'
+    silentArgs     = '/qn /norestart'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ tachidesk-server/update.ps1
@@ -0,0 +1,33 @@
+Import-Module au
+
+$releases = 'https://github.com/Suwayomi/Tachidesk-Server/releases'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url32   = $download_page.links | Where-Object href -match '-x86.msi$' | ForEach-Object href | Select-Object -First 1
+    $url64   = $download_page.links | Where-Object href -match '-x64.msi$' | ForEach-Object href | Select-Object -First 1
+    $version = (Split-Path ( Split-Path $url64 ) -Leaf).Split('-')[1]
+
+    @{
+        URL32   = 'https://github.com' + $url32
+        URL64   = 'https://github.com' + $url64
+        Version = $version
+    }
+}
+
+Update-Package -ChecksumFor All
