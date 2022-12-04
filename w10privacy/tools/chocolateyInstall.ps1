b9803c649465214430994390b120917edbfe40d8
Chocolatey <chocolatey@realdimensions.net>
2022-12-04 03:01:37 +0000

    AU: 1 updated - w10privacy [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/94db79380aff5b2db64dd7c3bf0cc009bfa77a8d

--- w10privacy/tools/chocolateyInstall.ps1
+++ w10privacy/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion
 $packageArgs = @{
   packageName    = 'w10privacy'
   unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-  url64Bit       = 'https://sf91b3285d9193eec.jimcontent.com/download/version/1669225339/module/12302828636/name/W10Privacy.zip'
-  checksum64     = '5ac1a69260e7fa8a5d3678cbd9f8f643144daf95642bdf232ee01a2ae8bb5ece'
+  url64Bit       = 'https://sf91b3285d9193eec.jimcontent.com/download/version/1670056258/module/12302828636/name/W10Privacy.zip'
+  checksum64     = '6ce1aa9887f4c56b57a62b9d25d4b8cc8a434fb58b740207e5c94fbcd7ca6890'
   checksumType64 = 'sha256'
 }
 
