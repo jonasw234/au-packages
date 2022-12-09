496c5e839e7c76dc01fa44d480261353abd53537
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2022-12-09 11:25:19 +0100

    Updated version check

--- crewlink/update.ps1
+++ crewlink/update.ps1
@@ -1,7 +1,7 @@
 Import-Module au
 . $PSScriptRoot\..\_scripts\all.ps1
 
-$releases = 'https://github.com/ottomated/CrewLink/releases'
+$releases = 'https://api.github.com/repos/ottomated/CrewLink/releases/latest'
 
 function global:au_SearchReplace {
    @{
@@ -18,13 +18,13 @@ function global:au_GetLatest {
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re      = 'CrewLink-Setup-\d+(\.\d+)*.exe'
-    $url     = $download_page.links | Where-Object href -match $re | ForEach-Object href | Select-Object -First 1
+    $re      = 'CrewLink-Setup-\d+(\.\d+)*.exe$'
+    $url     = (($download_page.Content | ConvertFrom-Json).assets | Where-Object browser_download_url -match $re).browser_download_url
     $version = $url -split '\/v|\/' + $re
     $version = $version[1]
 
     @{
-        URL     = 'https://github.com' + $url
+        URL     = $url
         Version = $version
     }
 }
