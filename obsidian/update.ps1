8b82dec80d0afd1a169a1e723b2ff4281335a9a5
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2022-12-09 11:36:05 +0100

    Updated version check

--- obsidian/update.ps1
+++ obsidian/update.ps1
@@ -1,13 +1,15 @@
 Import-Module au
 . $PSScriptRoot\..\_scripts\all.ps1
 
-$releases = 'https://github.com/obsidianmd/obsidian-releases/releases'
+$releases = 'https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest'
 
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -18,15 +20,17 @@ function global:au_GetLatest {
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re      = 'Obsidian\.([^-]*).*\.exe'
-    $domain  = $releases -split '(?<=//.+)/' | Select-Object -First 1
-    $url     = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
-    $url     = $url | ForEach-Object {$domain + $_ }
-    $version = $url -match $re
+    $re32    = 'Obsidian\.([^-]*).*32\.exe'
+    $json    = ($download_page.Content | ConvertFrom-Json).assets
+    $url32   = ($json | Where-Object browser_download_url -match $re32).browser_download_url
+    $re64    = 'Obsidian\.([^-]*).*64\.exe'
+    $url64   = ($json | Where-Object browser_download_url -match $re64).browser_download_url
+    $version = $url32 -match $re32
     $version = $matches[1]
 
     @{
-        URL     = $url
+        URL32   = $url32
+        URL64   = $url64
         Version = $version
     }
 }
