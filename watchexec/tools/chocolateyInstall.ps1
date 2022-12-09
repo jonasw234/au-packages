c8dd38d200e480bf14bf07cbf1bbd3dd12742e38
Chocolatey <chocolatey@realdimensions.net>
2022-12-09 10:12:40 +0000

    AU: 1 updated - watchexec [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/4c267e299857d2b21ebb3d019bef792be05fd076

--- watchexec/tools/chocolateyInstall.ps1
+++ watchexec/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64Bit       = 'https://github.com/watchexec/watchexec/releases/download/cli-v1.20.5/watchexec-1.20.5-x86_64-pc-windows-msvc.zip'
-    checksum64     = '1e440070b1fa7528cd333d725e5b7c35d7851746bc3a1a8edf10cc3e4094fe6c'
+    url64Bit       = 'https://github.com/watchexec/watchexec/releases/download/v1.20.6/watchexec-1.20.6-x86_64-pc-windows-msvc.zip'
+    checksum64     = 'f625f33830d77384cd65b594d30ca96aff62402a5e1a4fa0d100312ccad2e1aa'
     checksumType64 = 'sha256'
 }
 
