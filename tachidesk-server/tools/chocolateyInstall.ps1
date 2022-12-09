3979c03fb659070dbb816b32cd0b4b67762ef064
Chocolatey <chocolatey@realdimensions.net>
2022-12-09 09:28:16 +0000

    AU: 1 updated - tachidesk-server [skip ci] https://gist.github.com/235ac14f598c6ee89f066ba309bdf5a7/c7f91b1baa0ead2ca68ed49d8f111a8fd792382d

--- tachidesk-server/tools/chocolateyInstall.ps1
+++ tachidesk-server/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
     packageName    = $packageName
     fileType       = 'msi'
-    url            = 'https://github.com/Suwayomi/Tachidesk-Server/releases/download/v0.6.5/Tachidesk-Server-v0.6.5-r1122-windows-x86.msi'
-    checksum       = 'be05c88df93682d427bda713c47f9601cf9126fa61c0702bc22445ff41048df2'
+    url            = 'https://github.com/Suwayomi/Tachidesk-Server/releases/download/v0.6.6/Tachidesk-Server-v0.6.6-r1159-windows-x86.msi'
+    checksum       = '3977344dbf901646584882e7003f9d324c0f22b66157b9f9b865f635ddfa9b7c'
     checksumType   = 'sha256'
-    url64bit       = 'https://github.com/Suwayomi/Tachidesk-Server/releases/download/v0.6.5/Tachidesk-Server-v0.6.5-r1122-windows-x64.msi'
-    checksum64     = '0a5bfbc4ef1a22fd903adb3de4f81f893eeddd170b844b7f7d5d83af6c9462e1'
+    url64bit       = 'https://github.com/Suwayomi/Tachidesk-Server/releases/download/v0.6.6/Tachidesk-Server-v0.6.6-r1159-windows-x64.msi'
+    checksum64     = '6db49fb99e850bbcb4aa94a0977dea4cefad81a275ce5016e5d8b04059451432'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
 }
