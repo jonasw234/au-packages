c1ac41d8fd0ad40829a4b0492de6f4841938c871
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2022-12-09 11:10:56 +0100

    Updated version check

--- watchexec/update.ps1
+++ watchexec/update.ps1
@@ -1,6 +1,6 @@
 import-module au
 
-$releases = 'https://github.com/watchexec/watchexec/releases'
+$releases = 'https://api.github.com/repos/watchexec/watchexec/releases/latest'
 
 function global:au_SearchReplace {
    @{
@@ -17,12 +17,12 @@ function global:au_GetLatest {
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $url     = $download_page.links | Where-Object href -match '-x86_64-pc-windows-(gnu|msvc)\.zip$' | ForEach-Object href | Select-Object -First 1
-    $version = $url -split '-v|/watchexec'
-    $version = $version[3]
+    $url     = (($download_page.Content | ConvertFrom-Json).assets | Where-Object browser_download_url -match '-x86_64-pc-windows-(gnu|msvc)\.zip$').browser_download_url
+    $version = $url -split '-v|/watchexec-|-'
+    $version = $version[1]
 
     return @{
-        URL     = 'https://github.com' + $url
+        URL     = $url
         Version = $version
     }
 }
