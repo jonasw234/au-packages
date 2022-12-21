e2c173d313700025c56b1875d7b9f54a863248ae
John Cohen <76706095+jcohen-io@users.noreply.github.com>
2022-12-21 09:47:07 -0800

    Update chocolateyInstall.ps1
    fixed 64bit link & sha256 checksum.

--- obsidian/tools/chocolateyInstall.ps1
+++ obsidian/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
   url            = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.1.8/Obsidian.1.1.8-32.exe'
   checksum       = 'afb9f375e5aa54066555aefcd99fc46e5910cdfdda166f6098882bb79aa4ed55'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.1.8/Obsidian.1.1.8-arm64.exe'
-  checksum64     = '4e545d88bc6e78462e9bea438efb17f4954163790932181b55b274de325b8cb7'
+  url64bit       = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.1.8/Obsidian.1.1.8.exe'
+  checksum64     = 'E926ACF88F6552CC096868CD1806202A873D8603E3400AA4D78035ADC0375DEF'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
 }
