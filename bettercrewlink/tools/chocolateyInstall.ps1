2486a15fa5af0e8480b3ca7b31244f13cc2bac56
Chocolatey <chocolatey@realdimensions.net>
2023-03-29 03:01:37 +0000

    AU: 1 updated - bettercrewlink [skip ci] https://gist.github.com/jonasw234/235ac14f598c6ee89f066ba309bdf5a7/37508b2a2ee6d2a905e203d50a96a508fe6198d6

--- bettercrewlink/tools/chocolateyInstall.ps1
+++ bettercrewlink/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
-  url          = 'https://github.com/OhMyGuus/BetterCrewLink/releases/download/v3.1.1/Better-CrewLink-Setup-3.1.1.exe'
-  checksum     = '299f540f9bfc0fe589ceff868b65107e0e8421116bbcff8f859f4b242105281c'
+  url          = 'https://github.com/OhMyGuus/BetterCrewLink/releases/download/v3.1.2/Better-CrewLink-Setup-3.1.2.exe'
+  checksum     = '345f715a4e8a3b3e98f46c2f380d8951a68ac00caf30defddb8c2fdaf9f16b3a'
   checksumType = 'sha256'
   silentArgs   = '/S'
 }
