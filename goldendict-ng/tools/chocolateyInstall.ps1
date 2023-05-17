f64219486df53a6d3cc6e708652f392da61f8660
Jonas Wendorf <15037703+jonasw234@users.noreply.github.com>
2023-05-17 15:32:35 +0200

    Added GoldenDict-ng

---
+++ goldendict-ng/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:chocolateyPackageName
+
+$packageArgs = @{
+    packageName  = $packageName
+    fileType     = 'exe'
+    url          = 'https://github.com/xiaoyifang/goldendict-ng/releases/download/v23.05.01-WuYi.230501.6c8b0c1c/6.4.3-GoldenDict-ng-v23.05.01-WuYi-Installer.exe'
+    checksum     = '8c5d474e02ef3ec1aee38590179d388d6ce7f4a8c8bef0b8a8de2d3b24fe6cad'
+    checksumType = 'sha256'
+    silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ goldendict-ng/update.ps1
@@ -0,0 +1,33 @@
+Import-Module au
+. $PSScriptRoot\..\_scripts\all.ps1
+
+$releases = 'https://api.github.com/repos/xiaoyifang/goldendict-ng/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re32 = '\d+(\.\d+)*-GoldenDict-ng-v(\d+(\.\d+)*)-WuYi-Installer\.exe'
+    $json = ($download_page.Content | ConvertFrom-Json).assets
+    $url32 = ($json | Where-Object browser_download_url -match $re32).browser_download_url | Select-Object -Last 1
+    $version = $url32 -match $re32
+    $version = $matches[2]
+
+    @{
+        URL32   = $url32
+        Version = $version
+    }
+}
+
+Update-Package -ChecksumFor 32
