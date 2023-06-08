a9488bbe16e88f96df1682560ceedb1ecd570e2e
Chocolatey <chocolatey@realdimensions.net>
2023-06-08 03:01:34 +0000

    AU: 1 updated - freeoffice [skip ci] https://gist.github.com/jonasw234/235ac14f598c6ee89f066ba309bdf5a7/df68e25b2553480ce86be59bb5fc6cd1ff460e65

--- freeoffice/tools/chocolateyInstall.ps1
+++ freeoffice/tools/chocolateyInstall.ps1
@@ -11,7 +11,7 @@ $packageArgs = @{
     packageName    = $packageName
     fileType       = 'msi'
     url            = 'https://www.freeoffice.com/download.php?filename=https://www.softmaker.net/down/freeoffice2021.msi'
-    checksum       = 'e80fcdff9b7af8f7f1b06df3f8c6bded6a8cdb343f4ff3f5ad292c74b19c46ac'
+    checksum       = '71b78f12f8ccce3b2ddc6b4eadb6b8395390252880d3e735fa80bbe6b64b19aa'
     checksumType   = 'sha256'
     silentArgs     = '/norestart /qn APPLICATIONFOLDER="' + $installDir + '"'
 }
