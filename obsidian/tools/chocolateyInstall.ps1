249824920a9c944b40998cab7344b926413eeb2c
Chocolatey <chocolatey@realdimensions.net>
2023-09-01 03:02:11 +0000

    AU: 1 updated - obsidian [skip ci] https://gist.github.com/jonasw234/235ac14f598c6ee89f066ba309bdf5a7/cd15a7dc16f8301795c503f499a6e2b1bfbffd6b

--- obsidian/tools/chocolateyInstall.ps1
+++ obsidian/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.3.7/Obsidian.1.3.7-32.exe'
-  checksum       = '1100ea2503bac68ca6a2d57ee7ee09fda5058ceaee9f437ced226585541b9639'
+  url            = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.5/Obsidian.1.4.5-32.exe'
+  checksum       = '0f68d47632c700e54d6a019a8d2b555b485af43aa88ec7e1cb98b7cdaa7a0d6b'
   checksumType   = 'sha256'
-  url64bit       = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.3.7/Obsidian.1.3.7.exe'
-  checksum64     = '7e11b8c6e9e5f7723e22be3e36b7d49a6f67fffd12a77b3913978cd264ea7de2'
+  url64bit       = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.5/Obsidian.1.4.5.exe'
+  checksum64     = 'a5995a610a25f843128ed6ef6fe1bd2b0102f8549edf0b5d81dc82ba638e0074'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
 }
