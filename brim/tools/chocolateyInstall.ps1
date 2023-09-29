3bf1ac32ff4207f2842325d0225f3aad23af0d77
Chocolatey <chocolatey@realdimensions.net>
2023-09-29 03:01:47 +0000

    AU: 1 updated - brim [skip ci] https://gist.github.com/jonasw234/235ac14f598c6ee89f066ba309bdf5a7/107b5af03d6ac69c52cffa9eed830a78b7c7c365

--- brim/tools/chocolateyInstall.ps1
+++ brim/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
-  url          = 'https://github.com/brimdata/zui/releases/download/v1.2.0/Zui-Setup-1.2.0.exe'
-  checksum     = 'd74f268e9a33e5e3c5dc1d9b4e1e7f8fff612814dd53cbe16e64f070834290f6'
+  url          = 'https://github.com/brimdata/zui/releases/download/v1.3.0/Zui-Setup-1.3.0.exe'
+  checksum     = '38bb9077def7aca1ecb5c0fab00e96dc0c41543b6e6d6541295687f2bcaac1a0'
   checksumType = 'sha256'
   silentArgs   = '/S'
 }
