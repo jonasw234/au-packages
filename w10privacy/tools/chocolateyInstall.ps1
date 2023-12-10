16dba10fb997cbea43e5021f9839ca7eafa52200
Chocolatey <chocolatey@realdimensions.net>
2023-12-10 03:01:29 +0000

    AU: 2 updated - w10privacy watchexec [skip ci] https://gist.github.com/jonasw234/235ac14f598c6ee89f066ba309bdf5a7/fe0a9f6faee4266b637c292fb51860015d3626fe

--- w10privacy/tools/chocolateyInstall.ps1
+++ w10privacy/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $download_dir = "$Env:TEMP\chocolatey\$packageName\$Env:ChocolateyPackageVersion
 $packageArgs = @{
   packageName    = 'w10privacy'
   unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-  url64Bit       = 'https://sf91b3285d9193eec.jimcontent.com/download/version/1701285455/module/12302828636/name/W10Privacy.zip'
-  checksum64     = '4a1422ae7de63d4f8b1f4938c22685f7b906db1b0bb08d4b2460743b2009be98'
+  url64Bit       = 'https://sf91b3285d9193eec.jimcontent.com/download/version/1702114721/module/12302828636/name/W10Privacy.zip'
+  checksum64     = '26788e47d678f5d390d5fef2727630a6660d2820f7de85e60c07b40b84ef0f8c'
   checksumType64 = 'sha256'
 }
 
--- watchexec/tools/chocolateyInstall.ps1
+++ watchexec/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64Bit       = 'https://github.com/watchexec/watchexec/releases/download/v1.23.0/watchexec-1.23.0-x86_64-pc-windows-msvc.zip'
-    checksum64     = '394ba47e582044a065ccfe7acf951b30fa25bf003a246050aa84772c864ad94b'
+    url64Bit       = 'https://github.com/watchexec/watchexec/releases/download/v1.24.0/watchexec-1.24.0-x86_64-pc-windows-msvc.zip'
+    checksum64     = '4c091bcd814ff3ab712fec94d281ac1a6db899de3507167944eca461b9349ff8'
     checksumType64 = 'sha256'
 }
 
