82b43e02e5b61fc522dd57409f41c40a604ced82
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2024-01-15 08:07:31 +0100

    Fixed Czkawka hash

--- czkawka/tools/chocolateyInstall.ps1
+++ czkawka/tools/chocolateyInstall.ps1
@@ -1,18 +1,18 @@
-﻿$ErrorActionPreference  = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
-$packageToolsDir        = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$toolsDir               = Get-ToolsLocation
+$packageToolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = Get-ToolsLocation
 
-$installPath            = Join-Path $toolsDir 'czkawka'
-$binPath                = Join-Path $installPath 'czkawka_gui.exe'
-$iconPath               = Join-Path $packageToolsDir 'icon_about.ico'
+$installPath = Join-Path $toolsDir 'czkawka'
+$binPath = Join-Path $installPath 'czkawka_gui.exe'
+$iconPath = Join-Path $packageToolsDir 'icon_about.ico'
 
-$desktopShortcutPath    = "$($ENV:PUBLIC)\Desktop\Czkawka GUI.lnk"
-$startmenuShortcutPath  = "$($ENV:ProgramData)\Microsoft\Windows\Start Menu\Programs\Czkawka GUI.lnk"
+$desktopShortcutPath = "$($ENV:PUBLIC)\Desktop\Czkawka GUI.lnk"
+$startmenuShortcutPath = "$($ENV:ProgramData)\Microsoft\Windows\Start Menu\Programs\Czkawka GUI.lnk"
 
-$url64                  = 'https://github.com/qarmin/czkawka/releases/download/6.1.0/windows_czkawka_gui.zip'
-$checksum64             = '369b735b40860dc6e84634245cc386fe9d03fc1739a5283684bddcbc282dac65'
-$checksumType64         = 'sha256'
+$url64 = 'https://github.com/qarmin/czkawka/releases/download/6.1.0/windows_czkawka_gui.zip'
+$checksum64 = 'AB2B6FFF3059CEE27116F13985A06A53CA46627FEA2478E8949BF504AE50A144'
+$checksumType64 = 'sha256'
 
 $packageName = $env:chocolateyPackageName
 
