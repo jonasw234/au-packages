c94e1655adb55a54812108b942c5a5aadf50cb61
Chocolatey <chocolatey@realdimensions.net>
2024-02-06 03:01:28 +0000

    AU: 1 updated - gimagereader [skip ci] https://gist.github.com/jonasw234/235ac14f598c6ee89f066ba309bdf5a7/e43168ce38660e65f58ffd0ffbe8fbf839967096

--- gimagereader/tools/chocolateyInstall.ps1
+++ gimagereader/tools/chocolateyInstall.ps1
@@ -8,8 +8,8 @@ $packageArgs = @{
     # url            = 'https://github.com/manisandro/gImageReader/releases/download/v3.3.1/gImageReader_3.3.1_qt5_i686.exe'
     # checksum       = 'A363F282CF44E839C3163EFA00D363A7372A84432B396F380C2A50241F45DD44'
     # checksumType   = 'sha256'
-    url64bit       = 'https://github.com/manisandro/gImageReader/releases/download/v3.4.1/gImageReader_3.4.1_qt5_x86_64.exe'
-    checksum64     = '017b5f63664ac0636f60a8a1140b9ff2cd351dc2ad1c160eb0d91e1d1555e2b9'
+    url64bit       = 'https://github.com/manisandro/gImageReader/releases/download/v3.4.2/gImageReader_3.4.2_qt5_x86_64.exe'
+    checksum64     = 'db959a95534e29845f8954ac82c88f18b0b20c5e01cfe0720765718d96d7781f'
     checksumType64 = 'sha256'
     silentArgs     = '/S'
 }
