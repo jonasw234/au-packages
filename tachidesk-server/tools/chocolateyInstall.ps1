32f2e3a70780ae5d8024b3cc9104e5fc4a291361
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2024-02-21 20:23:38 +0100

    Include pre-releases

--- tachidesk-server/tools/chocolateyInstall.ps1
+++ tachidesk-server/tools/chocolateyInstall.ps1
@@ -6,10 +6,10 @@ $packageArgs = @{
     packageName    = $packageName
     fileType       = 'msi'
     url            = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v0.7.0-r1493/Suwayomi-Server-v0.7.0-r1493-windows-x86.msi'
-    checksum       = ''
+    checksum       = '9076625d856028c3ef908efd960afd14b4954e93f42534fb02b3df4008697a6f'
     checksumType   = 'sha256'
     url64bit       = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v0.7.0-r1493/Suwayomi-Server-v0.7.0-r1493-windows-x64.msi'
-    checksum64     = ''
+    checksum64     = '78989959b7ddb4eb09af9bd22921baee46edb159950aba9155149d263892565b'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
 }
