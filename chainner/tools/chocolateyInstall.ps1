20d9135de98ce5bb5eb4fd98cc4b95d977d12d39
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2024-02-25 21:20:11 +0100

    Add chaiNNer

---
+++ chainner/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = $env:chocolateyPackageName
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'exe'
+  url64bit       = 'https://github.com/chaiNNer-org/chaiNNer/releases/download/v0.22.0/chaiNNer-0.22.0-x64-windows-setup.exe'
+  checksum64     = '13985af82cfe9dee6b1445d26ec8a45c92e28bb4968929388db91c152ca36318'
+  checksumType64 = 'sha256'
+  silentArgs     = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ chainner/update.ps1
@@ -0,0 +1,31 @@
+Import-Module au
+
+$releases = 'https://api.github.com/repos/chainner-org/chainner/releases/latest'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(^[$]url64\s*=\s*)('.*')" = "`$1'$( $Latest.URL64 )'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$( $Latest.Checksum64 )'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $AllProtocols = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+    [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $file    = '-x64-windows-setup\.exe$'
+    $url     = (($download_page.Content | ConvertFrom-Json).assets | Where-Object browser_download_url -Match $file).browser_download_url
+    $version = $url -split '\/download\/|\/' + $file
+    $version = $version[1]
+
+    return @{
+        URL64   = $url
+        Version = $version
+    }
+}
+
+Update-Package -ChecksumFor 64
