eea95a4e61e943c98ba99c15a9d43a8918e9283d
Chocolatey <chocolatey@realdimensions.net>
2024-04-29 03:02:09 +0000

    AU: 2 updated - tachidesk-server watchexec [skip ci] https://gist.github.com/jonasw234/235ac14f598c6ee89f066ba309bdf5a7/2e688406f9b522b573cea01b3c6b3aca602c9ecd

--- tachidesk-server/tools/chocolateyInstall.ps1
+++ tachidesk-server/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
     packageName    = $packageName
     fileType       = 'msi'
-    url            = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.0.0-r1517/Suwayomi-Server-v1.0.0-r1517-windows-x86.msi'
-    checksum       = '661cc9943a429f50af5fa2f2575ef85cf21bf8ce2f4568e05d41035ece8bf4a4'
+    url            = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.0.0-r1519/Suwayomi-Server-v1.0.0-r1519-windows-x86.msi'
+    checksum       = '4051a5c1b203cc373ddba3c892f97b26f2bd751c491e79a01bcfca1e5db76408'
     checksumType   = 'sha256'
-    url64bit       = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.0.0-r1517/Suwayomi-Server-v1.0.0-r1517-windows-x64.msi'
-    checksum64     = '39999729713c2c3e9b8f81e308151d19c1c0821edc59fa118ccf0c501a6ec01b'
+    url64bit       = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.0.0-r1519/Suwayomi-Server-v1.0.0-r1519-windows-x64.msi'
+    checksum64     = '7a4e93441483b9fcf928e700c6df3d9bd52632e38b8f562efb1b296fe7c19075'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
 }
--- watchexec/tools/chocolateyInstall.ps1
+++ watchexec/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-    url64Bit       = 'https://github.com/watchexec/watchexec/releases/download/v2.0.0/watchexec-2.0.0-x86_64-pc-windows-msvc.zip'
-    checksum64     = '9e5121902c66755e68b038373e6f99ad7e41a784029a31729542886da516e2c2'
+    url64Bit       = 'https://github.com/watchexec/watchexec/releases/download/v2.1.0/watchexec-2.1.0-x86_64-pc-windows-msvc.zip'
+    checksum64     = 'e7b7d0e7ba6968c475af4ea2741f0ad990b64c7974d97ee0aee855c4e5192ca6'
     checksumType64 = 'sha256'
 }
 
