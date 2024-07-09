627a51e2974fd840f517bc821fd552246d4ca274
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2024-07-09 18:17:18 +0200

    chore: Updated Obsidian version

--- obsidian/update.ps1
+++ obsidian/update.ps1
@@ -6,8 +6,6 @@ $releases = 'https://api.github.com/repos/obsidianmd/obsidian-releases/releases/
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(?i)(^\s*url64bit\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
