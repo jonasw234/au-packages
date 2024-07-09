50cf43095979c65f60dd325671e145b92737194a
Jonas A. Wendorf <15037703+jonasw234@users.noreply.github.com>
2024-07-09 17:56:15 +0200

    fix: No more x86 support for Obsidian

--- obsidian/tools/chocolateyInstall.ps1
+++ obsidian/tools/chocolateyInstall.ps1
@@ -5,11 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName    = $packageName
   fileType       = 'exe'
-  url            = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.3/Obsidian.1.6.3-32.exe'
-  checksum       = 'dc327055f55a73846e47d8229bf91395ddd4b23b129f3acff75580da5b9097be'
-  checksumType   = 'sha256'
-  url64bit       = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.3/Obsidian.1.6.3.exe'
-  checksum64     = 'e4489273eb27325f1b08ed1c5f210924c49a50ca617099d1ff69dd922eef00dd'
+  url64bit       = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.6.5/Obsidian-1.6.5.exe'
+  checksum64     = '22c1e1d1c303fda74a84488db5f25263b1fd1c9c9d9390f0dfc075f208cb0006'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
 }
