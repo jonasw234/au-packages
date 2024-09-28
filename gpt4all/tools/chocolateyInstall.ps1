2354828878c7b9f71cf70ee2e1b790e012cac5a6
Chocolatey <chocolatey@realdimensions.net>
2024-09-28 03:02:05 +0000

    AU: 1 updated - gpt4all [skip ci] https://gist.github.com/jonasw234/235ac14f598c6ee89f066ba309bdf5a7/95c385de95214c0aa3cad72ca7035a108340cd01

--- gpt4all/tools/chocolateyInstall.ps1
+++ gpt4all/tools/chocolateyInstall.ps1
@@ -13,8 +13,8 @@ if ($key.Count -ge 1)
 $packageArgs = @{
     packageName    = $packageName
     fileType       = 'exe'
-    url64          = 'https://github.com/nomic-ai/gpt4all/releases/download/v3.3.0/gpt4all-installer-win64-v3.3.0.exe'
-    checksum64     = '42ee24184c74ee3d898488281eb495b1928351ef6b09e75dee5e0f28c5835265'
+    url64          = 'https://github.com/nomic-ai/gpt4all/releases/download/v3.3.1/gpt4all-installer-win64-v3.3.1.exe'
+    checksum64     = '82329fc1be48535bbe50fe28ba47ab3c9f45e899fbd51d9f87a7b54750e46adb'
     checksumType64 = 'sha256'
     silentArgs     = $installString + ' --accept-licenses --default-answer --confirm-command'
 }
