07a87822c58093fa557f3ed3658855d279746a34
Chocolatey <chocolatey@realdimensions.net>
2024-11-16 03:03:02 +0000

    AU: 1 updated - tachidesk-server [skip ci] https://gist.github.com/jonasw234/235ac14f598c6ee89f066ba309bdf5a7/781d94cbf850430ee75365418c4fd91ed037583a

--- tachidesk-server/tools/chocolateyInstall.ps1
+++ tachidesk-server/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
     packageName    = $packageName
     fileType       = 'msi'
-    url            = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.1.1-r1610/Suwayomi-Server-v1.1.1-r1610-windows-x86.msi'
-    checksum       = '78ab2c8f5c979288997a221d9043e7b401448e1516008aeaeccd434fd8f62dff'
+    url            = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.1.1-r1618/Suwayomi-Server-v1.1.1-r1618-windows-x86.msi'
+    checksum       = '068d51949f16e26a0310ba4af2165da6cc7d97418c38f8226aa1790ee5e7be42'
     checksumType   = 'sha256'
-    url64bit       = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.1.1-r1610/Suwayomi-Server-v1.1.1-r1610-windows-x64.msi'
-    checksum64     = 'c5efaaf0296990785bbd8b06f62437f38c5a2ec75e49e91b2478eebbe11631ce'
+    url64bit       = 'https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/v1.1.1-r1618/Suwayomi-Server-v1.1.1-r1618-windows-x64.msi'
+    checksum64     = '5bab28d38039922af9350db87f01307fb3fc84420171ef9ada082097b5580f2c'
     checksumType64 = 'sha256'
     silentArgs     = '/qn /norestart'
 }
